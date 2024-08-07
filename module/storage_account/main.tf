5650a732d45a3087d0f884b3adbea4315dca5687
bipin <kumarbipin247@gmail.com>
2024-08-07 21:28:38 +0530

    Corrected code

--- module/storage_account/main.tf
+++ module/storage_account/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_storage_account" "storage" {
   name=each.value.storage_account_name
   resource_group_name= each.value.resource_group_name
   location=each.value.location
-  account_tier=each.value.access_tier
+  account_tier=each.value.account_tier
   account_replication_type=each.value.account_replication_type
 }
 
