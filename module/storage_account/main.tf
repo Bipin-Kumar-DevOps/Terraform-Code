a7916a65f54797dd8ad62fc071e4526802ff2f4e
bipin <kumarbipin247@gmail.com>
2024-08-07 08:58:41 +0530

    Updated code

--- module/storage_account/main.tf
+++ module/storage_account/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_stroage_account" "storage" {
+resource "azurerm_storage_account" "storage" {
   for_each = var.storage
   name=each.value.storage_account_name
   resource_group_name= each.value.resource_group_name
