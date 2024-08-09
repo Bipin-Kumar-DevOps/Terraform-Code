0a50d0d85cac46401380a50efac07193298588d2
bipin <kumarbipin247@gmail.com>
2024-08-09 21:02:19 +0530

    Updated backend

--- env/test/provider.tf
+++ env/test/provider.tf
@@ -9,7 +9,7 @@ terraform {
        resource_group_name  = "prashantrg"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
     storage_account_name = "pksa452"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "pk9452"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "bipin-dev.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    key                  = "bipin-test.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
 }
 
